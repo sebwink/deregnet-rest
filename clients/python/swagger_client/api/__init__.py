f5cc2ec80bf3bd54e3baaea8ad005abecad62ad6
sebwink <winkler@informatik.uni-tuebingen.de>
2018-01-24 22:36:00 +0100

    python client init; fixed broken python client

---
+++ clients/python/swagger_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.graph_api import GraphApi
+from swagger_client.api.info_api import InfoApi
+from swagger_client.api.nodeset_api import NodesetApi
+from swagger_client.api.parameter_set_api import ParameterSetApi
+from swagger_client.api.run_api import RunApi
+from swagger_client.api.score_api import ScoreApi
+from swagger_client.api.subgraph_api import SubgraphApi
