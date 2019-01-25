8d78659992c92ff6b71e107d9bb21a92545d566d
sebwink <winkler@informatik.uni-tuebingen.de>
2019-01-25 14:16:49 +0100

    ...

--- compose.sh
+++ compose.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-export DEREGNET_PORT=8181
-
-export KONG_PG_DATABASE=test
-export KONG_PG_USER=test
-export KONG_PG_PASSWORD=test123
+export KONG_PG_DATABASE=kongdb
+export KONG_PG_USER=kongdb
+export KONG_PG_PASSWORD=dev
 
 docker-compose $@
--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-curl -X POST \
+echo "Register deregnet service"
+curl --silent -X POST \
      --url http://kong:8001/services \
      --data 'name=deregnet' \
      --data 'url=http://deregnet-rest:8080/deregnet/' > /dev/null
 
-curl -X POST \
+echo "Setting route and path of deregnet service"
+curl --silent -X POST \
      --url http://kong:8001/services/deregnet/routes \
-     --data 'hosts[]=dereg.net' \
-     --data 'paths[]=/deregnet'
+     --data 'paths[]=/deregnet' \
+     --data 'methods[]=GET' \
+     --data 'methods[]=POST' \
+     --data 'methods[]=DELETE' > /dev/null
