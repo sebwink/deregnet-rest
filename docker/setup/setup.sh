929f65c52d59463850b0d5aa002f92b6bc269c3c
sebwink <winkler@informatik.uni-tuebingen.de>
2019-01-29 13:47:32 +0100

    docs

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -22,6 +22,18 @@ curl --silent -X POST \
      --data 'name=deregnet' \
      --data 'url=http://deregnet-rest:8080/deregnet/' > /dev/null
 
+echo "Register something else"
+curl --silent -X POST \
+     --url http://kong:8001/services \
+     --data 'name=something-else' \
+     --data 'url=http://something-else:5000/' > /dev/null
+
+echo "Register deregnet docs"
+curl --silent -X POST \
+     --url http://kong:8001/services \
+     --data 'name=deregnet-docs' \
+     --data 'url=http://deregnet-docs:5000/' > /dev/null
+
 echo "Setting route and path of deregnet service"
 curl --silent -X POST \
      --url http://kong:8001/services/deregnet/routes \
@@ -30,11 +42,31 @@ curl --silent -X POST \
      --data 'methods[]=POST' \
      --data 'methods[]=DELETE' > /dev/null
 
+echo "Setting route and path of something-else service"
+curl --silent -X POST \
+     --url http://kong:8001/services/something-else/routes \
+     --data 'paths[]=/deregnet/something' \
+     --data 'methods[]=GET' \
+     --data 'methods[]=POST' \
+     --data 'methods[]=DELETE' > /dev/null
+
+echo "Setting route and path of documentation service"
+curl --silent -X POST \
+     --url http://kong:8001/services/deregnet-docs/routes \
+     --data 'paths[]=/docs/deregnet/' \
+     --data 'methods[]=GET' > /dev/null
+
 # TODO: anonymous access?
 
 echo "Enabling basic-auth for deregnet service"
 curl --silent -X POST \
      --url http://kong:8001/services/deregnet/plugins \
+     --data "name=basic-auth" \
+     --data "config.hide_credentials=true" > /dev/null
+
+echo "Enabling basic-auth for something-else service"
+curl --silent -X POST \
+     --url http://kong:8001/services/something-else/plugins \
      --data "name=basic-auth" \
      --data "config.hide_credentials=true" > /dev/null
 
